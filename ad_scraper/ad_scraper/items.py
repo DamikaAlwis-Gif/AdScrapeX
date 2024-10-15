1eb3f881ecb5af0b70a764bc81b7771833ec5a39
DamikaAlwis <damikaa.20@cse.mrt.ac.lk>
2024-10-15 13:02:37 +0530

    Refactor ad_scraper/items.py and ad_scraper/pipelines.py

--- ad_scraper/ad_scraper/items.py
+++ ad_scraper/ad_scraper/items.py
@@ -9,4 +9,13 @@ import scrapy
 class AdScraperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    ad_title = scrapy.Field()
+    breadcrumbs = scrapy.Field()
+    more_details = scrapy.Field()
+    features = scrapy.Field()
+    description = scrapy.Field()
+    date_time = scrapy.Field()
+    ad_url = scrapy.Field()
+    images_urls = scrapy.Field()
+
+    
