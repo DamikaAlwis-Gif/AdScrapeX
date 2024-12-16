6d9607131f5d4b3167d451ce13cafb921b41bd9c
Pamudu Chathunka <119758027+pamuduchat@users.noreply.github.com>
2024-12-16 21:13:49 +0530

    update  spider to change start URL, implement user agent rotation, and add download delay

--- ad_scraper/ad_scraper/spiders/riyasewana.py
+++ ad_scraper/ad_scraper/spiders/riyasewana.py
@@ -2,11 +2,13 @@ import scrapy
 from scrapy_playwright.page import PageMethod
 from ..utils import genaral
 from urllib.parse import urljoin
+import time
+import random
 
 class RiyasewanaSpider(scrapy.Spider):
     name = "riyasewana"
     allowed_domains = ["riyasewana.com"]
-    start_urls = ["https://riyasewana.com/search/vans"]
+    start_urls = ["https://riyasewana.com/search/suvs"]
 
     custom_settings = {
         "FEEDS": {
@@ -19,14 +21,25 @@ class RiyasewanaSpider(scrapy.Spider):
         #     "headless": False,  # Set this to True if you want headless mode
         # },
         "PLAYWRIGHT_ABORT_REQUEST": genaral.should_abort_request,
+        "JOBDIR": "job_data/riyasewana_job",
+        "DOWNLOAD_DELAY": random.uniform(1, 3), 
 
     }
+
+        # List of user agents to rotate
+    user_agents = [
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        # Add more user agents as needed
+    ]
+
 
     def start_requests(self):
         url = self.start_urls[-1]
         yield scrapy.Request(
             url=url,
             callback=self.parse,
+            headers={'User-Agent': random.choice(self.user_agents)},
             meta= {
                 "playwright": True,
                 "playwright_page_methods": [
@@ -35,8 +48,9 @@ class RiyasewanaSpider(scrapy.Spider):
                 ],
             }
         )
+        time.sleep(random.uniform(1, 3))
 
-    def parse(self, response):
+    async def parse(self, response):
         self.logger.info(f"Parsing {response.url}")
         ad_urls = response.xpath(
             "//ul/li[contains(@class,'item')]/h2[contains(@class,'more')]/a/@href").getall()
@@ -44,12 +58,13 @@ class RiyasewanaSpider(scrapy.Spider):
             yield scrapy.Request(
                 url=ad_url,
                 callback=self.parse_ad,
+                headers={'User-Agent': random.choice(self.user_agents)},
                 meta={
                     "ad_url": ad_url,
                     "playwright": True,
                     "playwright_page_methods": [
                         PageMethod(
-                            "wait_for_selector", "//div[@id='content']")
+                            "wait_for_selector", "//div[@id='content']", timeout=60000)
                     ],
                 }
             )
@@ -58,6 +73,7 @@ class RiyasewanaSpider(scrapy.Spider):
             yield scrapy.Request(
                 url= urljoin("https:",next_page_url),
                 callback=self.parse,
+                headers={'User-Agent': random.choice(self.user_agents)},
                 meta= {
                     "playwright": True,
                     "playwright_page_methods": [
